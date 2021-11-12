27110cd371b14ecfc448cc4cf51437105d86c716
anwar@uexcelerate.com <uexcelerate_123>
2021-11-12 09:00:19 +0530

    first commit

---
+++ src/main/java/com/anwar/tool/git_demo_repo/App.java
@@ -0,0 +1,13 @@
+package com.anwar.tool.git_demo_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
